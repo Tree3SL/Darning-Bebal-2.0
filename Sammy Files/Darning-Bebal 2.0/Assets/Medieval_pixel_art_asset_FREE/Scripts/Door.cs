815e467be1b9a53c1192eafd6806042396d14af8
Sami Terracciano <sterracc@uci.edu>
2021-02-09 16:40:54 -0800

    Working Build

---
+++ Sammy Files/Darning-Bebal 2.0/Assets/Medieval_pixel_art_asset_FREE/Scripts/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public enum DoorEnum
+    {
+        Left,
+        Right,
+    }
+
+    public DoorEnum doorEnum;
+    public GameObject endPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public DoorEnum GetDoorEnum()
+    {
+        return doorEnum;
+    }
+
+    public GameObject GetObjEnd()
+    {
+        return endPosition;
+    }
+}
---
+++ Sammy Files/Darning-Bebal 2.0/Assets/Medieval_pixel_art_asset_FREE/Scripts/PlayerMove.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5.0f;
+    public GameObject target;
+    public bool isIn = false;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isIn)
+        {
+            this.transform.localPosition = target.transform.localPosition;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        Vector3 dx = new Vector3(inputX, 0.0f, 0.0f);
+        transform.position += dx * speed * Time.deltaTime; 
+     
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Door"))
+        {
+            switch (collision.GetComponent<Door>().GetDoorEnum())
+            {
+                case Door.DoorEnum.Left:
+                    target = collision.GetComponent<Door>().GetObjEnd();
+                    isIn = true;
+                    break;
+                case Door.DoorEnum.Right:
+                    target = collision.GetComponent<Door>().GetObjEnd();
+                    isIn = true;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Door"))
+        {
+            switch (collision.GetComponent<Door>().GetDoorEnum())
+            {
+                case Door.DoorEnum.Left:
+                    target = collision.GetComponent<Door>().GetObjEnd();
+                    isIn = true;
+                    break;
+                case Door.DoorEnum.Right:
+                    target = collision.GetComponent<Door>().GetObjEnd();
+                    isIn = true;
+                    break;
+                default:
+                    break;
+            }
+           
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Door"))
+        {
+            target = null;
+            isIn = false;
+        }
+    }
+}
