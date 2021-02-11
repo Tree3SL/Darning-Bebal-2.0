602ee6a9146e91f17edb1f642be37f85bd833e56
dshdd <syc418@gmail.com>
2021-02-11 14:34:00 -0800

    Added Quests
    1. integrate the public project folder (removed from Sam's file).
    2. Added all 4 Quests to project (tested, removed from Alan's file).

---
+++ Darning-Bebal 2.0/Assets/Scripts/Quest/quest_MemoryGameKey.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quest_MemoryGameKey : MonoBehaviour
+{
+    public bool isRight = false;
+    public bool isHide = false;
+    public Sprite hidden_sprite;
+    public Sprite show_sprite;
+
+
+    public bool gameStart = false;
+
+    private IEnumerator coroutine;
+
+    void Start()
+    {
+        this.GetComponent<Image>().sprite = show_sprite;
+    }
+
+
+    public IEnumerator WaitAndHide(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        Hide();
+    }
+
+    public void Reveal() {
+        isHide = false;
+        this.GetComponent<Image>().sprite = show_sprite;
+        if (!isRight) {
+            coroutine = WaitAndHide(0.5f);
+            StartCoroutine(coroutine);
+        }
+    }
+
+    public void Hide() {
+        isHide = true;
+        this.GetComponent<Image>().sprite = hidden_sprite;
+    }
+
+}
---
+++ Darning-Bebal 2.0/Assets/Scripts/Quest/quest_QTE.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quest_QTE : MonoBehaviour
+{
+    Slider ProgressBar;
+    public GameObject input_Group;
+    public GameObject key_prefab;
+
+    public string[] test_key;
+    public Sprite[] key_sprite;
+    public int[] keys_per_level;
+
+    public List<string> current_keys;
+    public List<GameObject> current_key_objects;
+    public int current_index = 0;
+    public int current_level = 0;
+
+    public Color success_input;
+    public Color fail_input;
+
+    bool isDone = false;
+
+    void Start()
+    {
+        ProgressBar = GetComponent<Slider>();
+        if (ProgressBar != null)
+        {
+            ProgressBar.value = 0;
+        }
+        current_keys = new List<string>();
+
+        CreateKeyList();
+    }
+
+    void CreateKeyList()
+    {
+        for (int i = 0; i < keys_per_level[current_level]; i++)
+        {
+            int index = Random.Range(0, test_key.Length - 1);
+            current_keys.Add(test_key[index]);
+
+            GameObject new_key_object = Instantiate(key_prefab);
+            new_key_object.transform.SetParent(input_Group.transform);
+            new_key_object.GetComponent<Image>().sprite = key_sprite[index];
+
+            current_key_objects.Add(new_key_object);
+        }
+    }
+
+    void ClearKeyList() 
+    {
+        for (int i = 0; i < current_key_objects.Count; i++)
+        {
+            if(current_key_objects[i]!=null)  Destroy(current_key_objects[i]);
+        }
+        current_key_objects.Clear();
+        current_keys.Clear();
+
+    }
+
+    void Update()
+    {
+        if (!isDone) 
+        {
+            if (current_index < keys_per_level[current_level])
+            {
+                if (Input.anyKey)
+                {
+                    //input correct
+                    if (Input.GetKeyDown(current_keys[current_index]))
+                    {
+                        Input.ResetInputAxes();
+                        current_key_objects[current_index].GetComponent<Image>().color = success_input;
+                        current_index++;
+                        CheckState();
+                    }
+                    //input fail
+                    else
+                    {
+                        for (int i = 0; i <= current_index; i++)
+                        {
+                            current_key_objects[i].GetComponent<Image>().color = fail_input;
+                        }
+                        current_index = 0;
+                        Input.ResetInputAxes();
+                    }
+                }
+            }
+        }
+        
+    }
+
+    public void CheckState()
+    {
+        if (ProgressBar != null)
+        {
+            if (current_index >= keys_per_level[current_level])
+            {
+                current_index = 0;
+                current_level++;
+                BarIncrement();
+                if (current_level >= keys_per_level.Length)
+                {
+                    ClearKeyList();
+                    Debug.Log("QTE Quest Condition Met");
+                    isDone = true;
+                }
+                else
+                {
+                    ClearKeyList();
+                    CreateKeyList();
+                }
+            }
+        }
+    }
+
+    public void BarIncrement() 
+    {
+        if (ProgressBar != null)
+        {
+            ProgressBar.value += ProgressBar.maxValue/ keys_per_level.Length;
+        }
+        
+    }
+
+}
