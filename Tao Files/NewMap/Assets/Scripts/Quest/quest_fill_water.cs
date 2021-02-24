4c7d540d16850cf844ca264dceae52d090a3cf8c
Tao <tying4@uci.edu>
2021-02-25 00:09:06 +0800

    NewMap

---
+++ Tao Files/NewMap/Assets/Scripts/Quest/quest_fill_water.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quest_fill_water : MonoBehaviour
+{
+    Slider ProgressBar;
+    public float Fill_Speed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ProgressBar = GetComponent<Slider>();
+        if (ProgressBar != null) 
+        {
+            ProgressBar.value = 0;
+        }
+    }
+    void Update()
+    {
+        if (ProgressBar == null) return;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            ProgressBar.value += Fill_Speed * Time.deltaTime;
+        }
+        else 
+        {
+            if (ProgressBar.value >= ProgressBar.maxValue)
+            {
+                Debug.Log("Water Fill Condition Met");
+            }
+        }
+    }
+}
